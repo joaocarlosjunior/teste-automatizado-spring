0fa3e68ac4aeacdb2cf141e05e9d6fbcb6577e2a
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-25 16:27:17 -0300

    feat: adicionado planet controller

---
+++ src/main/java/com/joaocarlos/api_planetas_start_wars/controllers/PlanetsController.java
@@ -0,0 +1,35 @@
+package com.joaocarlos.api_planetas_start_wars.controllers;
+
+import com.joaocarlos.api_planetas_start_wars.domain.Planet;
+import com.joaocarlos.api_planetas_start_wars.services.PlanetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetsController {
+    private final PlanetService planetService;
+
+    public PlanetsController(PlanetService planetService) {
+        this.planetService = planetService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(planetService.createPlanet(planet));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Planet> getPlanetById(@PathVariable Long id) {
+        return planetService.getPlanetById(id).map(planet -> ResponseEntity.ok(planet)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Planet>> getPlanets() {
+        return ResponseEntity.ok(planetService.listPlanets());
+    }
+
+}
