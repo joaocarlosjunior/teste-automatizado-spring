989d42d58db3dfae1033cfc691f5299f78453585
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-25 12:00:27 -0300

    initial commit

---
+++ src/main/java/com/joaocarlos/api_planetas_start_wars/ApiPlanetaStarWarsApplication.java
@@ -0,0 +1,13 @@
+package com.joaocarlos.api_planetas_start_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanetaStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanetaStarWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/ApiPlanetaStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocarlos.api_planetas_start_wars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanetaStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
