2e78e7c93693b96ea336af4826783fb276dbd52c
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-25 16:27:49 -0300

    feat: adicionado planet service

---
+++ src/main/java/com/joaocarlos/api_planetas_start_wars/services/PlanetService.java
@@ -0,0 +1,29 @@
+package com.joaocarlos.api_planetas_start_wars.services;
+
+import com.joaocarlos.api_planetas_start_wars.domain.Planet;
+import com.joaocarlos.api_planetas_start_wars.repositories.PlanetRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PlanetService {
+    private final PlanetRespository planetRespository;
+
+    public PlanetService(PlanetRespository planetRespository) {
+        this.planetRespository = planetRespository;
+    }
+
+    public Planet createPlanet(Planet planet) {
+        return this.planetRespository.save(planet);
+    }
+
+    public Optional<Planet> getPlanetById(Long id) {
+        return this.planetRespository.findById(id);
+    }
+
+    public List<Planet> listPlanets() {
+        return this.planetRespository.findAll();
+    }
+}
