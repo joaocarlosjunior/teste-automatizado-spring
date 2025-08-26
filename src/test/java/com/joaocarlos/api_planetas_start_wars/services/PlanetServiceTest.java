c0bef6273695075e1714516f46cce44a39f1f8f2
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-26 16:56:21 -0300

    feat: adicionado teste para metodos busca filtrada e delete

--- src/test/java/com/joaocarlos/api_planetas_start_wars/services/PlanetServiceTest.java
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/services/PlanetServiceTest.java
@@ -1,5 +1,6 @@
 package com.joaocarlos.api_planetas_start_wars.services;
 
+import com.joaocarlos.api_planetas_start_wars.builder.QueryBuilder;
 import com.joaocarlos.api_planetas_start_wars.domain.Planet;
 import com.joaocarlos.api_planetas_start_wars.repositories.PlanetRespository;
 import org.junit.jupiter.api.Test;
@@ -7,13 +8,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.INVALID_PLANET;
 import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.PLANET;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -80,5 +85,36 @@ public class PlanetServiceTest {
         Optional<Planet> sut = planetService.getPlanetByName("name");
 
         assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets(){
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+        when(planetRespository.findAll(query)).thenReturn(List.of(PLANET));
+
+        List<Planet> sut = planetService.filterByClimateOrTerrain("climate", "terrain");
+
+        assertThat(sut).hasSize(1);
+        assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void listPlanets_ReturnsEmptyList(){
+        when(planetRespository.findAll(any(Example.class))).thenReturn(Collections.emptyList());
+
+        List<Planet> sut = planetService.filterByClimateOrTerrain(PLANET.getClimate(), PLANET.getTerrain());
+
+        assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void deletePlanet_WithExistingId_DoesNotThrowAnyException() {
+        assertThatCode(() -> planetService.delete(1L)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void deletePlanet_WithUnexistingId_ThrowsException() {
+        doThrow(new RuntimeException()).when(planetRespository).deleteById(99L);
+        assertThatThrownBy(() -> planetService.delete(99L)).isInstanceOf(RuntimeException.class);
     }
 }
