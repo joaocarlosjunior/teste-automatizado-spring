609df9e675a396548d64d320c12243404e3431bd
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-28 16:33:19 -0300

    feat: adicionado testes criacao e busca de planetas

--- src/test/java/com/joaocarlos/api_planetas_start_wars/controllers/PlanetControllerTest.java
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/controllers/PlanetControllerTest.java
@@ -5,13 +5,18 @@ import com.joaocarlos.api_planetas_start_wars.services.PlanetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.INVALID_PLANET;
 import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,15 +38,43 @@ public class PlanetControllerTest {
         when(planetService.createPlanet(PLANET)).thenReturn(PLANET);
 
         // Act e Assert
-        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$").value(PLANET));
     }
 
     @Test
-    public void createPlanet_WithInvalidData_ReturnsBadRequest() throws Exception {
-        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(INVALID_PLANET))
+    public void createPlanet_WithInvalidData_ReturnsUnprocessableEntity() throws Exception {
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(INVALID_PLANET)).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        when(planetService.createPlanet(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets")
+                        .content(objectMapper.writeValueAsString(PLANET))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isUnprocessableEntity());
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
+        when(planetService.getPlanetById(1L)).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(get("/planets/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty() throws Exception {
+        when(planetService.getPlanetById(99L)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/planets/{id}", 99L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
