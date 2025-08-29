90f6ef8fbe33cad6c68404ed393a3068a8a8530f
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-29 18:00:32 -0300

    feat: adicionado insercao de planeta para teste

---
+++ src/test/resources/sql/import_planets.sql
@@ -0,0 +1,6 @@
+INSERT INTO tbl_planets(name, climate, terrain)
+VALUES ('Tatooine', 'arid', 'desert');
+INSERT INTO tbl_planets(name, climate, terrain)
+VALUES ('Alderaan', 'temperate', 'grasslans, mountains');
+INSERT INTO tbl_planets(name, climate, terrain)
+VALUES ('Yavin IV', 'temperate, tropical', 'jungle, rainforest');
