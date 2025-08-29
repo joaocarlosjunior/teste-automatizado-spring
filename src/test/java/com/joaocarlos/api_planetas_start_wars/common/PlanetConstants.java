8691ce7ba297502b5f5fd15638c27d136e27a152
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-08-29 17:59:28 -0300

    feat: adicionado mais exemplos de planetas

--- src/test/java/com/joaocarlos/api_planetas_start_wars/common/PlanetConstants.java
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/common/PlanetConstants.java
@@ -2,7 +2,14 @@ package com.joaocarlos.api_planetas_start_wars.common;
 
 import com.joaocarlos.api_planetas_start_wars.domain.Planet;
 
+import java.util.List;
+
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
+
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grassland, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainforest");
+    public static final List<Planet> PLANETS = List.of(TATOOINE, ALDERAAN, YAVINIV);
 }
