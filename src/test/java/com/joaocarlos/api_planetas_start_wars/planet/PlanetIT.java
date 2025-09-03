f2b095b86643f2a4653fe7065debd135f7954367
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-03 17:24:57 -0300

    feat: adicionado teste subcutaneo criacao planeta

---
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/planet/PlanetIT.java
@@ -0,0 +1,33 @@
+package com.joaocarlos.api_planetas_start_wars.planet;
+
+import com.joaocarlos.api_planetas_start_wars.domain.Planet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/sql/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void createPlanet_ReturnsPlanet(){
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+    }
+}
