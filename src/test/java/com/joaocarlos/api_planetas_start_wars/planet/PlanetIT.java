76ccdb7dca4644e8369d42afed3f0fb9f5919f96
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-04 16:31:11 -0300

    feat: adicionado teste busca planeta pelo id

--- src/test/java/com/joaocarlos/api_planetas_start_wars/planet/PlanetIT.java
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/planet/PlanetIT.java
@@ -11,10 +11,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.PLANET;
+import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.TATOOINE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/sql/import_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 @Sql(scripts = {"/sql/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
     @Autowired
@@ -29,5 +31,14 @@ public class PlanetIT {
         assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
         assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+    }
+
+    @Test
+    public void getPlanets_ReturnsPlanets(){
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
     }
 }
