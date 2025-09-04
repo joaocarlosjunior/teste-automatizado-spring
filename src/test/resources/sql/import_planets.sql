a43cf25b0a8c6405c65c5e5c99143656889a4921
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-04 16:31:20 -0300

    feat: adicionado campo id

--- src/test/resources/sql/import_planets.sql
+++ src/test/resources/sql/import_planets.sql
@@ -1,6 +1,6 @@
-INSERT INTO tbl_planets(name, climate, terrain)
-VALUES ('Tatooine', 'arid', 'desert');
-INSERT INTO tbl_planets(name, climate, terrain)
-VALUES ('Alderaan', 'temperate', 'grasslans, mountains');
-INSERT INTO tbl_planets(name, climate, terrain)
-VALUES ('Yavin IV', 'temperate, tropical', 'jungle, rainforest');
+INSERT INTO tbl_planets(id, name, climate, terrain)
+VALUES (1, 'Tatooine', 'arid', 'desert');
+INSERT INTO tbl_planets(id, name, climate, terrain)
+VALUES (2, 'Alderaan', 'temperate', 'grasslans, mountains');
+INSERT INTO tbl_planets(id, name, climate, terrain)
+VALUES (3, 'Yavin IV', 'temperate, tropical', 'jungle, rainforest');
