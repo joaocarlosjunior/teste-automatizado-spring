2d0ff43c714040df6b7bd4352f2fd0c4c6fce152
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-08 17:02:57 -0300

    feat: adicionado testes subcutaneo listagem e remocao planetas

--- src/test/java/com/joaocarlos/api_planetas_start_wars/planet/PlanetIT.java
+++ src/test/java/com/joaocarlos/api_planetas_start_wars/planet/PlanetIT.java
@@ -5,25 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.PLANET;
-import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.TATOOINE;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.joaocarlos.api_planetas_start_wars.common.PlanetConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = {"/sql/import_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = {"/sql/import_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = {"/sql/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void createPlanet_ReturnsPlanet(){
+    public void createPlanet_ReturnsPlanet() {
         ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
@@ -34,11 +39,67 @@ public class PlanetIT {
     }
 
     @Test
-    public void getPlanets_ReturnsPlanets(){
+    public void getPlanets_ReturnsPlanets() {
         ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(sut.getBody().getId()).isNotNull();
         assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void getPlanetByName_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/name/" + TATOOINE.getName(), Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets", Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).hasSize(3);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ByClimate_ReturnsPlanets(){
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("/planets")
+                .queryParam("climate", "arid");
+
+        ResponseEntity<List<Planet>> sut = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Planet>>() {
+                });
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isNotNull();
+        assertThat(sut.getBody().get(0)).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ByTerrain_ReturnsPlanets(){
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets?terrain=" + YAVINIV.getTerrain(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).hasSize(1);
+        assertThat(Arrays.stream(sut.getBody()).findFirst().get()).isEqualTo(YAVINIV);
+    }
+
+    @Test
+    public void removePlanet_ReturnsNoContent() {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("/planets/77");
+
+        ResponseEntity<Void> sut = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.DELETE,
+                null,
+                new ParameterizedTypeReference<Void>() {
+                });
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 }
